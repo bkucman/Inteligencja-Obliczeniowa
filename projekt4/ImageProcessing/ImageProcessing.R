b642dc51da20811c831a04d3144f1527692b5ab6
Błażej <kucmeno12@o2.pl>
2019-01-22 02:08:07 +0100

    add proj4

--- projekt4/ImageProcessing/ImageProcessing.R
+++ projekt4/ImageProcessing/ImageProcessing.R
@@ -1,25 +1,29 @@
 cran <- getOption("repos")
 cran["dmlc"] <- "https://apache-mxnet.s3-accelerate.dualstack.amazonaws.com/R/CRAN/"
 options(repos = cran)
+
+install.packages("BiocManager")
+BiocManager::install("EBImage")
+
 install.packages("mxnet")
-
 install.packages("pbapply")
+install.packages("caret")
+install.packages("randomForest")
+install.packages("gbm")
 
+library(mxnet)
+library(pbapply)
+library(EBImage)
+library(caret)
+library(randomForest)
+library(gbm)
+library(e1071)
+
+# windows
 image_dirNormal <- "..\\data\\chest_xray\\train\\NORMAL"
 image_dirPneumonia <- "..\\data\\chest_xray\\train\\PNEUMONIA"
 test_image_dirNormal <- "..\\data\\chest_xray\\test\\NORMAL"
 test_image_dirPneumonia <- "..\\data\\chest_xray\\test\\PNEUMONIA"
-
-install.packages("BiocManager")
-BiocManager::install("EBImage")
-library(EBImage)
-
-exampleNormal <- readImage(file.path(image_dirNormal, "IM-0115-0001.jpeg"))
-display(exampleNormal)
-examplePneumonia <- readImage(file.path(image_dirPneumonia, "person1_bacteria_1.jpeg"))
-display(examplePneumonia)
-
-display(resize(examplePneumonia, w = width, h = height))
 
 width <- 28
 height <- 28
@@ -64,25 +68,153 @@ extract_feature <- function(dir_path, width, height, is_pneumonia = TRUE, add_la
   return(feature_matrix)
 }
 
+# calculating train images to vector of pixels
 pneumonia_data <- extract_feature(dir_path = image_dirPneumonia, width = width, height = height)
 normal_data <- extract_feature(dir_path = image_dirNormal, width = width, height = height, is_pneumonia = FALSE)
-dim(cats_data)
+dim(pneumonia_data)
 
+# calculating test images to vector of pixels
 pneumonia_test_data <- extract_feature(dir_path = test_image_dirPneumonia, width = width, height = height)
 normal_test_data <- extract_feature(dir_path = test_image_dirNormal, width = width, height = height, is_pneumonia = FALSE)
 
+# save ready matrix with vectors of images
 saveRDS(pneumonia_data, "pneumonia.rds")
 saveRDS(normal_data, "normal.rds")
 saveRDS(pneumonia_test_data, "pneumonia_test.rds")
-saveRDS(normal_tes_data, "normal_test.rds")
+saveRDS(normal_test_data, "normal_test.rds")
 
-install.packages("caret")
-library(caret)
+## Bind test and trains rows in a single dataset
+complete_set <- rbind(pneumonia_data, normal_data)
+complete_set_test <- rbind(pneumonia_test_data, normal_test_data)
 
-## Bind rows in a single dataset
-complete_set <- rbind(cats_data, dogs_data)
-## test/training partitions
-training_index <- createDataPartition(complete_set$label, p = .9, times = 1)
-training_index <- unlist(training_index)
-train_set <- complete_set[training_index,]
-dim(train_set)
+dim(complete_set)
+dim(complete_set_test)
+
+# save matrix as csv
+#write.csv(complete_set, file = "comletedata.csv")
+#write.csv(complete_set_test, file = "comletedatatest.csv")
+
+####### Fix train and test datasets for CNN (mxnet library) #############
+train_data <- data.matrix(complete_set)
+train_x <- t(train_data[, -1]) # only parameters without label
+train_y <- train_data[,1] # label only
+train_array <- train_x
+dim(train_array) <- c(28, 28, 1, ncol(train_x))
+
+test_data <- data.matrix(complete_set_test)
+test_x <- t(complete_set_test[,-1])
+test_y <- complete_set_test[,1]
+test_array <- test_x
+dim(test_array) <- c(28, 28, 1, ncol(test_x))
+
+#ncol(train_array)
+#nrow(train_array)
+#typeof(complete_set)
+#typeof(train_data)
+#typeof(train_array)
+#length(train_array)
+
+#### Convolutional Neural Network (CNN) ###################################
+
+## Model based on toturial Image Classification in R: MXNet - Shikun Li
+mx_data <- mx.symbol.Variable('data')
+## 1st convolutional layer 5x5 kernel and 20 filters.
+conv_1 <- mx.symbol.Convolution(data = mx_data, kernel = c(5, 5), num_filter = 20)
+tanh_1 <- mx.symbol.Activation(data = conv_1, act_type = "tanh")
+pool_1 <- mx.symbol.Pooling(data = tanh_1, pool_type = "max", kernel = c(2, 2), stride = c(2,2 ))
+## 2nd convolutional layer 5x5 kernel and 50 filters.
+conv_2 <- mx.symbol.Convolution(data = pool_1, kernel = c(5,5), num_filter = 50)
+tanh_2 <- mx.symbol.Activation(data = conv_2, act_type = "tanh")
+pool_2 <- mx.symbol.Pooling(data = tanh_2, pool_type = "max", kernel = c(2, 2), stride = c(2, 2))
+## 1st fully connected layer
+flat <- mx.symbol.Flatten(data = pool_2)
+fcl_1 <- mx.symbol.FullyConnected(data = flat, num_hidden = 500)
+tanh_3 <- mx.symbol.Activation(data = fcl_1, act_type = "tanh")
+## 2nd fully connected layer
+fcl_2 <- mx.symbol.FullyConnected(data = tanh_3, num_hidden = 2)
+## Output
+NN_model <- mx.symbol.SoftmaxOutput(data = fcl_2)
+
+## Set seed for reproducibility
+mx.set.seed(100)
+
+## Device used. 
+device <- mx.cpu()
+
+
+model_CNN <- mx.model.FeedForward.create(NN_model, X = train_array, y = train_y,
+                                     ctx = device,
+                                     num.round = 30,
+                                     array.batch.size = 100,
+                                     learning.rate = 0.05,
+                                     momentum = 0.9,
+                                     wd = 0.00001,
+                                     eval.metric = mx.metric.accuracy,
+                                     epoch.end.callback = mx.callback.log.train.metric(100))
+
+## Test CNN classifier
+predict_CNN <- predict(model, test_array)
+predicted_labels_CNN <- max.col(t(predict_CNN)) - 1
+table(test_data[, 1], predicted_labels_CNN)
+
+sum(diag(table(test_data[, 1], predicted_labels_CNN)))/624
+
+
+### Random Forest 
+
+data_for_forest <- read.csv("comletedata.csv")
+data_for_forest <- data_for_forest[,-1]
+
+data_for_forest_test <- read.csv("comletedatatest.csv")
+data_for_forest_test <- data_for_forest_test[,-1]
+
+# Prepare labels to classifier
+labels <- data_for_forest[,1]
+labels <- as.character(labels)
+labels <- as.factor(labels)
+# data
+data_ready <- data_for_forest[,-1]
+
+model_randomForest = randomForest(data, labels, ntree = 30, maxnodes = 30)
+
+predict_randomForest = predict(model_randomForest, data_for_forest_test[,-1], type = "class")
+table = table(predict_randomForest , data_for_forest_test[,1])
+sum(diag(table(data_for_forest_test[,1], predict_randomForest )))/624
+
+## Gradient boosting
+
+require(dplyr)
+
+labels_GBM <- data_for_forest$label 
+data_for_GBM <- select(data_for_forest, -label)
+
+model_GBM <- gbm.fit(
+  x = data_for_GBM,
+  y = labels_GBM,
+  distribution = "bernoulli",
+  shrinkage=0.01,
+  interaction.depth = 3,
+  n.minobsinnode = 10,
+  verbose=F,
+  n.trees = 2000
+)
+
+predicted.GBM <- predict(model_GBM, newdata = data_for_forest_test[,-1], n.trees = 1000,type = "response")
+
+predicted.GBM <- predict(object = model_GBM, newdata=data_for_forest_test[,-1],
+                         n.trees = 2000, type = "response")
+
+predicted.GBM_round <- round(predicted.GBM)
+
+table.GBM <- table(predicted.GBM_round, data_for_forest_test[,1])
+sum(diag(table(data_for_forest_test[,1], predicted.GBM_round)))/624
+
+# SVM  ###### dobre
+
+model.SVM <- svm(label ~ ., data = train_data, type='C-classification',kernel='linear',
+                 scale=FALSE)
+
+predicted.SVM <- predict(model.SVM, newdata = test_data)
+conf.matrix.SVM <- table(test_data[,1],predicted.SVM)
+
+sum(diag(table(test_data[, 1], predicted.SVM)))/624
